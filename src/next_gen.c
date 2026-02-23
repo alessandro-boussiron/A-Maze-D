5265d6d30f995c3cabfb68673cbec411724c20db
Alessandro Boussiron <alessandro.boussiron@epitech.eu>
2026-02-23 18:24:37 +0100

    [fix] coding style & pointer issues
    fix :
    - fixining current issues
    
    /!\ USE BUILTIN LINKEDLIST METHODS TO GET ELEMENTS

--- src/next_gen.c
+++ src/next_gen.c
@@ -11,13 +11,14 @@
 int next_gen(amazed_t *amazed, linked_list_t *robots)
 {
     int max_weight = 0;
-    int indice = -1;
-    amazed_room_t *cur_room = amazed->room_list->head;
+    int index = -1;
+    node_t *cur_room = amazed->room_list->head->data;
+    amazed_room_t *room = (amazed_room_t *)cur_room->data;
 
     if (!cur_room)
         return 84;
     while (cur_room) {
-        if (cur_room->weight = max_weight && cur_room->has_robot == 0) {
+        if (room->weight == max_weight && room->has_robot == 0) {
             my_putstr("tungtungtungsahur");
         }
     }
