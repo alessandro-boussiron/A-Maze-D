9267b2d8933aecec49178ad8b5b61d42f8ffd498
William Rouviere <william-byron.rouviere@epitech.eu>
2026-02-23 10:34:31 +0100

    [add] display
    add:
    adding a function in order to print each movement of each robot each iteration
    in order to understand what our algorithm do.

---
+++ src/print_robot_move.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2026
+** G-CPE-200-MPL-2-1-amazed-2
+** File description:
+** error_returns
+*/
+#include "../include/my.h"
+#include <unistd.h>
+
+int print_robot_move(int robot_number, int room_number)
+{
+    if (write(1, "P", 1) < 0)
+        return ERROR_CODE;
+    my_put_nbr(robot_number);
+    if (write(1, "-", 1) < 0)
+        return ERROR_CODE;
+    my_put_nbr(room_number);
+    if (write(1, "\n", 1) < 0)
+        return ERROR_CODE;
+    return 0;
+}
